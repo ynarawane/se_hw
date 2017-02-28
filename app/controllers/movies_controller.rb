9506d3e2bcec0a1f9da7f2084735ab85178076d3
Yashwardhan Narawane <yash8192@tamu.edu>
2017-02-28 05:19:56 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,12 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sorting = params[:sort];
+    if(@sorting == nil)
+      @movies = Movie.all
+    else
+      @movies = Movie.order(@sorting)
+    end
   end
 
   def new
